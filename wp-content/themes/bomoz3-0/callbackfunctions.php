48bf1d987f7d0d86ce3906e6989052ecba07b4a7
Jitendra2103 <jitendrabhavsar469@gmail.com>
2023-03-11 11:42:15 +0530

    response altered

--- wp-content/themes/bomoz3-0/callbackfunctions.php
+++ wp-content/themes/bomoz3-0/callbackfunctions.php
@@ -16,11 +16,55 @@ function getAllPosts()
             $data = [];
             $postId = get_the_ID();
             $data['id'] = $postId;
-            $data['title'] = get_post_meta($postId,'post_title');
+            $data['title'] = get_post_meta($postId, 'post_title')[0];
+            $data['author']  = get_the_author_meta('nickname');
             $image = get_field('thumbnail_image');
-            $thumbnailSrc  =esc_url($image['url']);
+            $thumbnailSrc  = esc_url($image['url']);
             $data['thumbnail'] = $thumbnailSrc;
-            $data['description'] = get_post_meta($postId,'blog_description');
+            $data['description']  = [];
+            if (have_rows('description_section')) :
+                while (have_rows('description_section')) : the_row();
+                    $details = [];
+                    $details['type'] = get_sub_field('type');
+                    if ($details['type'] == 'list') {
+                        $details['content'] = explode('.', get_sub_field('description'));
+                    } else {
+
+                        $details['content'] = get_sub_field('description');
+                    }
+                    array_push($data['description'], $details);
+                endwhile;
+            endif;
+
+            $data['display_images'] = [];
+            if (have_rows('display_images')) :
+                while (have_rows('display_images')) : the_row();
+                    $image = get_sub_field('image');
+                    $imageDetails = [];
+                    $imageDetails['url'] = esc_url($image['url']);
+                    $imageDetails['alt'] = esc_html($image['alt']);
+                    $imageDetails['title'] = esc_html($image['title']);
+                    array_push($data['display_images'],$imageDetails);
+                endwhile;
+            endif;
+            
+            $data['faq_details'] = [];
+            $data['faq_questions'] = [];
+            if (have_rows('faqs')) :
+                $faqQuestions = [];
+                while (have_rows('faqs')) : the_row();
+                    $faqDetails = [];
+                    $faqDetails['question'] = get_sub_field('question');
+                    $faqDetails['answer'] = get_sub_field('answer');
+                    array_push($faqQuestions,get_sub_field('question'));
+                    array_push($data['faq_details'],$faqDetails);
+                endwhile;
+                array_push($data['faq_questions'],$faqQuestions);
+            endif;
+            
+
+
+
             array_push($posts, $data);
         endwhile;
         wp_reset_postdata();
@@ -28,18 +72,18 @@ function getAllPosts()
     return $posts;
 }
 
-function describePost(WP_REST_Request $request){
+function describePost(WP_REST_Request $request)
+{
     $postId = $request->get_param('id');
-    if($postId == '' || empty($postId)){
+    if ($postId == '' || empty($postId)) {
 
-        $response = ['success'=> false,'message' => 'Please provide post id.','result' => [],'status'=> 200];
-    }else{
-        $post = get_post( $postId);
-        if(empty($post)){
-        $response = ['success'=> false,'message' => 'Post does not exists.','result' => [],'status'=> 200];
-        }else{
-            $response = ['success'=> true,'message' => 'Post data retrived successfully.','result' => $post,'status'=> 200];
-
+        $response = ['success' => false, 'message' => 'Please provide post id.', 'result' => [], 'status' => 200];
+    } else {
+        $post = get_post($postId);
+        if (empty($post)) {
+            $response = ['success' => false, 'message' => 'Post does not exists.', 'result' => [], 'status' => 200];
+        } else {
+            $response = ['success' => true, 'message' => 'Post data retrived successfully.', 'result' => $post, 'status' => 200];
         }
     }
     return $response;
